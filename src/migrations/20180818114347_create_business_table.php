3543943a81c1cafb143928e180ad576f44a950a8
saud-ahmad <saud.ahmad@gems.techverx.com>
2018-08-18 17:54:04 +0500

    Database Migration files

---
+++ src/migrations/20180818114347_create_business_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use \dott\migrations\Migration;
+
+class CreateBusinessTable extends Migration
+{
+    public function up()
+    {
+        $this->schema->create('business', function (Illuminate\Database\Schema\Blueprint $table) {
+
+            $table->increments('id');
+            $table->string('name');
+            $table->string('construction_year');
+            $table->enum('class', [1, 2, 3]);
+            $table->boolean('governmental');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        $this->schema->drop('business');
+    }
+}
